3326009515e690857e0aabe1f6f1aa77fcbbb26e
jpzicato <jpablozicato2020@gmail.com>
2023-11-27 19:54:36 -0300

    refactor: Add date validation to Task model

--- src/models/task.js
+++ src/models/task.js
@@ -16,6 +16,20 @@ const taskSchema = new Schema(
       type: String,
       required: true,
       trim: true,
+      validate: [
+        {
+          validator: value =>
+            /^(\d{4})[-](0[1-9]|1[0-2])[-](0[1-9]|[12][0-9]|3[01])$/.test(
+              value
+            ),
+          message:
+            'The date is not valid. It must be in the format YYYY-MM-DD, using - as separators',
+        },
+        {
+          validator: value => !(value < new Date().toISOString().split('T')[0]),
+          message: 'The date is not valid. Must not be before today',
+        },
+      ],
     },
     status: {
       type: String,
