64bb55003319b366e16b186ded558d4a0085e7e3
jpzicato <jpablozicato2020@gmail.com>
2023-12-06 11:07:47 -0300

    refactor: Move user validation functions to utils

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -2,6 +2,13 @@ import Joi from 'joi';
 
 const stringFieldValidation = Joi.string();
 
+export const passwordsMatchValidation = Joi.object({
+  password: stringFieldValidation,
+  repeatPassword: stringFieldValidation
+    .valid(Joi.ref('password'))
+    .messages({ 'any.only': 'Passwords must match' }),
+});
+
 export const updateUserValidation = Joi.object({
   name: stringFieldValidation.min(3).max(30),
   email: stringFieldValidation,
