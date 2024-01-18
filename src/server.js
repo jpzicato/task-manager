9660b800f91010a9d380b1f684172a0d6ee33473
jpzicato <jpablozicato2020@gmail.com>
2024-01-18 11:13:12 -0300

    chore: Enable URL-encoded form data handling in server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
-import express, { json } from 'express';
+import express, { json, urlencoded } from 'express';
 import { createServer } from 'http';
 import cors from 'cors';
 import schema from './schema/index.js';
@@ -27,6 +27,9 @@ app.use(
   auth,
   cors(),
   json(),
+  urlencoded({
+    extended: false,
+  }),
   expressMiddleware(server, {
     context: async ({ req: { userId, error } }) => ({
       userId,
