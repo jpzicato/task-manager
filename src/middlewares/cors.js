ec0b6d21e2530b9963a24782b9b6fc2345b99c0d
jpzicato <jpablozicato2020@gmail.com>
2024-01-24 08:00:14 -0300

    refactor: Add "OPTIONS" method in cors middleware

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -2,7 +2,7 @@ import cors from 'cors';
 
 export default cors({
   origin: /^http:\/\/(?:localhost|(?:\d{1,3}\.){3}\d{1,3}):(\d+)$/,
-  methods: ['GET', 'POST'],
+  methods: ['GET', 'POST', 'OPTIONS'],
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization'],
 });
